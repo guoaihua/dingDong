12c43bbec518818886dd2451dc8c4831cee8b8d5
zimingguo <zimingguo@tencent.com>
2022-03-21 17:50:21 +0800

    feat: update index

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import React, { useState } from 'react'
 import { MenuProps } from "@/types";
 
 import { getApp } from '@tarojs/taro'
+import { View, Text, Image } from '@tarojs/components'
 import './index.scss'
 
 const app = getApp()
@@ -60,55 +60,80 @@ const Menu: MenuProps[] = [
     pagePath: '../random/random'
   }
 ]
-
+const infos1 = "更多功能开发中";
+const infos2 = "拯救选择困难症和纠结患者";
 
 /** 内容更新组件 */
 
 const UpdateInfo = () => {
   return (
-    <View className="modal" wx:if="{{showUpdate}}" catchtap="closeModal">
-      <View className="card" catchtap="clickModal">
-        <image src="/imges/close.svg" className="closeModal" catchtap="closeModal"></image>
-        <View className="title">功能更新</View>
-        <View className="info">叮咚决策器「今天吃什么」功能更新</View>
-        <View className="info">我们新增了「附近美食」功能，随机挑选你附近的美食店，并能快速查看店家地址和点外卖啦～</View>
-        <image className="updateInfo" src="/imges/updateInfo.png"></image>
+    <View className='modal' onClick={closeModal}>
+      <View className='card' onClick={clickModal}>
+        <Image src='/images/home/close.svg' className='closeModal' onClick={closeModal}></Image>
+        <View className='title'>功能更新</View>
+        <View className='info'>叮咚决策器「今天吃什么」功能更新</View>
+        <View className='info'>我们新增了「附近美食」功能，随机挑选你附近的美食店，并能快速查看店家地址和点外卖啦～</View>
+        <Image className='updateInfo' src='/images/home/updateInfo.png'></Image>
       </View>
-
     </View>
   )
 }
 
 const Home = () => {
+  const [hidden, setHidden] = useState('hidden')
+  const [showUpdate, setShowUpdate] = useState(false)
+
+  /** 跳转到相应的详情页面 */
+  const gotoDetail = (e) => {
+    let url = e.currentTarget.dataset.url;
+    if (url) {
+      Taro.navigateTo({
+        url: url,
+        success: function (data) {
+        },
+        fail: function (err) {
+          console.log(err);
+        }
+      });
+    }
+  }
+
+
   return (
     <>
-      <View className="page_container {{showUpdate ? 'fix': ''}}">
-        <header id="header" ></header>
-        <image src="/imges/header.png" className="header" id="headerbg"></image>
-        <View className="container">
-          <View className="menu">
-            <View className="title">
-              <View className="name">叮咚决策器</View>
-              <View className="tips">不要纠结啦，帮你做决定!</View>
-              <View hover-className="hoverStatus">
-                <image src="/imges/setting.svg" className="setting" bindtap="gotoDetail" data-url="../setting/setting"></image>
+      <View className={showUpdate ? 'page_container  fix' : 'page_container'}>
+        <header id='header' ></header>
+
+        <Image src='/images/home/header.png' className='header' id='headerbg'></Image>
+        <View className='container'>
+          <View className='menu'>
+            <View className='title'>
+              <View className='name'>叮咚决策器</View>
+              <View className='tips'>不要纠结啦，帮你做决定!</View>
+              <View hover-className='hoverStatus'>
+                <Image src='/images/setting.svg' className='setting' onClick={gotoDetail} data-url='../setting/setting'></Image>
               </View>
 
             </View>
-            <View className="list">
-              <View wx:for="{{gamelist}}" hover-className="hoverStatus" className="gamelist {{item.classNamename}}" bindtap="gotoDetail" data-url="{{item.url}}">
-                <image src="{{item.imgsrc}}"></image>
-                <View className="itemname">{{ item.name }}</View>
-                <View className="itemtips"> {{ item.tips }}</View>
-              </View>
+            <View className='list'>
+              {
+                Menu.map((item, index) => (
+                  <View hover-className='hoverStatus' key={index} className={`gamelist ${item.className}`} onClick={gotoDetail} data-url={item.pagePath}>
+                    <Image src={item.imgSrc}></Image>
+                    <View className='itemname'>{item.name}</View>
+                    <View className='itemtips'> {item.tips}</View>
+                  </View>
+                ))
+              }
+
             </View>
 
-            <View className="infos">
-              <View className="tips1">{{ infos1 }}</View>
-              <View className="tips2">{{ infos2 }}</View>
-              <image src="/imges/snow.png"></image>
+            <View className='infos'>
+              <View className='tips1'>{infos1}</View>
+              <View className='tips2'>{infos2}</View>
+              <Image src='/images/home/snow.png'></Image>
             </View>
-            <View className="more">
+            <View className='more'>
             </View>
           </View>
         </View>
